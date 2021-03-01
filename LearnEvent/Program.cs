4e1ca6a924ae8665a5619b3ef379d9bd715927aa
Moon-Moooon <75983359+Moon-Moooon@users.noreply.github.com>
2021-03-01 20:05:34 +0300

    First commit

---
+++ LearnEvent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
